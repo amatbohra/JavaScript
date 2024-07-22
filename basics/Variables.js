d5df4362765f5a0e20228085fd07c1fc965f42e7
amatbohra <142864327+amatbohra@users.noreply.github.com>
2024-07-22 17:08:08 +0000

    variables

---
+++ basics/Variables.js
@@ -0,0 +1,9 @@
+const accountId = 12344
+let accountEmail = "amatbohra@gmail.com"
+accountCity = "mumbai"
+let accountState
+console.table([accountEmail, accountId, accountState, accountCity])
+
+/*'
+prefer not using var as it does not support blaock scope and functional scope
+*/
