38f9012e1935e1ffa66021d1b67719a98a85cc20
amatbohra <142864327+amatbohra@users.noreply.github.com>
2024-07-22 17:08:31 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,11 @@
+// number => 2 to pwr 53
+// string => ""
+// boolean => true/false
+// bigint => for really big numbers
+// symbol => unique
+// null => standalone value
+//  undefined => value not given 
+
+// object
+ console.log(typeof null); //object
+ console.log(typeof undefined); //undefined
