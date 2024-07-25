97c0d47badc57c91359107b90aaf85e363809af2
amatbohra <142864327+amatbohra@users.noreply.github.com>
2024-07-25 11:35:20 +0000

    Strings

---
+++ basics/Strings.js
@@ -0,0 +1,23 @@
+let name = "amat"
+let age = "20"
+console.log(`hello my name is ${name} and my age is ${age}`)//good way to concenate strings 
+
+let mystring = new String("hitest-hc-com")
+console.log(mystring.charAt(3));
+console.log(mystring.indexOf('c'));
+console.log(mystring.includes("-"));
+console.log(mystring.substring(2,6));//it wont inclue the end index value
+console.log(mystring.slice(4,8));//can also use negative indexes 
+console.log(mystring.split("-"));//covert it to an array with a specified seperator or blank space
+console.log(mystring.length);
+console.log(mystring.toUpperCase());
+
+const xyz = "   oolaaala   "
+console.log(xyz);
+console.log(xyz.trim());//it will remove the not wanted extra space 
+
+// there are many methods to manipulate strings...you can study them from mdn 
+
+
+
+
