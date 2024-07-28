aa7275e645c23996181c73d769e26ec4e00dd147
amatbohra <142864327+amatbohra@users.noreply.github.com>
2024-07-28 10:41:01 +0000

    nulish and terniary operator

--- controlflow/truthyandfalsy.js
+++ controlflow/truthyandfalsy.js
@@ -17,3 +17,16 @@ const arr = []
 if (arr.length === 0) {
     console.log("Array is empty");
 }
+const obj = {}
+if(Object.keys(obj).length===0){
+    console.log("Object is empty");//accessing keys makes it return array which further can be aplied length property of array to compare it with 0 
+}
+
+// Nullish Coalescing Operator(??)
+const questlike = null??10
+console.log(questlike);//10 gets printed...it is used in the case where u dont want null value to return and you have to keep other backup
+
+//terniary operator(?)
+// condition? true{}:false{}
+const isprice = 20
+isprice <= 20 ? console.log("yes price is less than or equal to 20"): console.log("price is more than 20");
