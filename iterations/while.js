6f5540e7c7d9a140716598b8b7b58ba89d679f03
amatbohra <142864327+amatbohra@users.noreply.github.com>
2024-07-29 13:44:16 +0000

    while

---
+++ iterations/while.js
@@ -0,0 +1,19 @@
+let i=0
+while (i<=10) {
+    // console.log(i);
+    i = i+2
+}
+
+let myarray = ["ironman","superman","thor"]
+let arr = 0
+while (arr<myarray.length) {
+    // console.log(`my hero is ${myarray[arr]}`);
+    arr++
+}
+
+let j=16
+do {
+    console.log(`value is ${j}`);
+    j++
+} while (j<=10);//value is 16
+//in dowhile the value is printed before the condition is checked 
